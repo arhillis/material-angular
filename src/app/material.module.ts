319d7de12a2746b68480aea1ef02dd528768e808
Ann Hillis <andriellerhillis@gmail.com>
2022-03-12 17:48:47 -0600

    Added some modules for the form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
-//import {MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatButtonModule, MatTableModule, MatPaginatorModule , MatSortModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule } from  '@angular/material';
+
+//import {MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatTableModule, MatPaginatorModule , MatSortModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatProgressSpinnerModule,} from  '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+    imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
+    exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule]
 })
 
 export class MaterialModule {}
